6ddbe4ee1f8f6e628c92a8597bf3702b4f909d82
Aleksandr <shaban4@yandex.ru>
2024-03-22 17:11:36 +0700

    fix bugs 22.03

--- dist/js/app.js
+++ dist/js/app.js
@@ -3392,6 +3392,21 @@
             };
             window.addEventListener("scroll", buttonToTop);
         }
+        document.addEventListener("click", (function(e) {
+            const target = e.target;
+            const ddWrapper = target.closest("[data-dropdown]");
+            const ddActive = document.querySelector("._dd-active");
+            if (ddWrapper) dropdownAction(e, ddWrapper, ddActive); else if (ddActive) ddActive.classList.remove("_dd-active");
+        }));
+        function dropdownAction(e, ddWrapper, ddActive) {
+            const target = e.target;
+            const ddButton = ddWrapper.querySelector("[data-dropdown-button]");
+            if (target == ddButton) {
+                if (ddActive && ddActive !== ddWrapper) ddActive.classList.remove("_dd-active");
+                ddWrapper.classList.toggle("_dd-active");
+                e.preventDefault();
+            }
+        }
         function isWebp() {
             function testWebP(callback) {
                 let webP = new Image;
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -237,3 +237,33 @@ if (document.querySelector('.broadcast')) {
    window.addEventListener('scroll', buttonToTop);
 }
 //#endregion
+
+//#region Функционал дропдаунов открыть\закрыть
+
+document.addEventListener("click", function (e) {
+   const target = e.target;
+   const ddWrapper = target.closest('[data-dropdown]');
+   const ddActive = document.querySelector('._dd-active');
+
+   if (ddWrapper) {
+      dropdownAction(e, ddWrapper, ddActive);
+   } else if (ddActive) {
+      ddActive.classList.remove('_dd-active');
+   }
+});
+
+function dropdownAction(e, ddWrapper, ddActive) {
+   const target = e.target;
+   const ddButton = ddWrapper.querySelector('[data-dropdown-button]');
+
+   if (target == ddButton) {
+      if (ddActive && ddActive !== ddWrapper) {
+         ddActive.classList.remove('_dd-active');
+      }
+
+      ddWrapper.classList.toggle('_dd-active');
+      e.preventDefault();
+   }
+}
+
+//#endregion
